3d68ced6bc760da04048fa43622f6cd203bd4752
Joel Uckelman <uckelman@nomic.net>
2023-11-07 22:00:04 +0000

    Removed cruft.

--- src/main.rs
+++ src/main.rs
@@ -82,7 +82,6 @@ fn routes(api: &str) -> Router<AppState> {
         .route(
             &format!("{api}/projects/:proj"),
             get(handlers::project_get)
-// TODO: create project
             .put(handlers::project_put)
         )
         .route(
