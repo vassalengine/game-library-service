dff3b854cd96ba0cfb474b9a5257bfc84e3fa378
Joel Uckelman <uckelman@nomic.net>
2023-11-09 23:48:23 +0000

    Heuristically generate title sort keys.

--- src/prod_core.rs
+++ src/prod_core.rs
@@ -511,6 +511,26 @@ WHERE user_id = ?
     Ok(())
 }
 
+fn split_title_sort_key<'a>(title: &'a str) -> (&'a str, Option<&'a str>) {
+    match title.split_once(' ') {
+        // Probably Spanish or French, "A" is not an article
+        Some(("A", rest)) if rest.starts_with("la") => (title, None),
+        // Put leading article at end
+        Some(("A", rest)) => (rest, Some("A")),
+        Some(("An", rest)) => (rest, Some("An")),
+        Some(("The", rest)) => (rest, Some("The")),
+        // Doesn't start with an article
+        Some(_) | None => (title, None)
+    }
+}
+
+fn title_sort_key(title: &str) -> String {
+    match split_title_sort_key(title) {
+        (_, None) => title.into(),
+        (rest, Some(art)) => format!("{rest}, {art}")
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
