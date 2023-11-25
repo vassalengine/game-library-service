c6bffc29935150609a9eafbbd757fca8e884c470
Joel Uckelman <uckelman@nomic.net>
2023-11-25 16:39:20 +0000

    Use the typedef, that's why we defined it.

--- src/app.rs
+++ src/app.rs
@@ -1,13 +1,12 @@
 use axum::extract::FromRef;
-use std::sync::Arc;
 
 use crate::{
-    core::Core,
+    core::CoreArc,
     jwt::DecodingKey
 };
 
 #[derive(Clone, FromRef)]
 pub struct AppState {
     pub key: DecodingKey,
-    pub core: Arc<dyn Core + Send + Sync>
+    pub core: CoreArc
 }
