24e80aa764d0722233f34f7ff39e733347a9c3a8
Joel Uckelman <uckelman@nomic.net>
2023-12-05 22:46:25 +0000

    Added test fixture for window functions.

---
+++ src/fixtures/proj_window.sql
@@ -0,0 +1,17 @@
+INSERT INTO projects (
+  id,
+  name,
+  description,
+  revision,
+  created_at,
+  modified_at,
+  game_title,
+  game_title_sort,
+  game_publisher,
+  game_year
+)
+VALUES
+  (1, "a", "", 1, "", "", "", "", "", ""),
+  (2, "b", "", 1, "", "", "", "", "", ""),
+  (3, "c", "", 1, "", "", "", "", "", ""),
+  (4, "d", "", 1, "", "", "", "", "", "");
