2ae6bbf07d98b9a5ad3a2d3e8b19ef8e61c93f50
Joel Uckelman <uckelman@nomic.net>
2024-04-13 16:55:56 +0100

    Don't generate a sort key here; better to suggest one in the frontend.

--- src/prod_core.rs
+++ src/prod_core.rs
@@ -143,9 +143,6 @@ where
     ) -> Result<(), CoreError>
     {
         let now = self.now_nanos()?;
-// TODO: generate a sort key?
-//        let mut proj_data = proj_data;
-//        proj_data.game.title_sort_key = title_sort_key(&proj_data.game.title);
         self.db.create_project(user, proj, proj_data, now).await
     }
 
@@ -756,26 +753,6 @@ impl TryFrom<ProjectSummaryRow> for ProjectSummary {
                 }
             }
         )
-    }
-}
-
-fn split_title_sort_key(title: &str) -> (&str, Option<&str>) {
-    match title.split_once(' ') {
-        // Probably Spanish or French, "A" is not an article
-        Some(("A", rest)) if rest.starts_with("la") => (title, None),
-        // Put leading article at end
-        Some(("A", rest)) => (rest, Some("A")),
-        Some(("An", rest)) => (rest, Some("An")),
-        Some(("The", rest)) => (rest, Some("The")),
-        // Doesn't start with an article
-        Some(_) | None => (title, None)
-    }
-}
-
-fn title_sort_key(title: &str) -> String {
-    match split_title_sort_key(title) {
-        (_, None) => title.into(),
-        (rest, Some(art)) => format!("{rest}, {art}")
     }
 }
 
