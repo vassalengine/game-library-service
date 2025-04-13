59911d6fa2d820a5ca1d14e6952f369b3b66925a
Joel Uckelman <uckelman@nomic.net>
2025-04-13 20:02:08 +0100

    Added hacky logging.

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use axum::{
     body::{Body, Bytes},
     extract::Request,
     http::StatusCode,
+    middleware::{self, Next},
     response::{IntoResponse, Json, Response},
     routing::{get, post}
 };
@@ -96,6 +97,55 @@ impl IntoResponse for AppError {
         let body = Json(HttpError::from(self));
         (code, body).into_response()
     }
+}
+
+async fn print_request_response(
+    req: Request,
+    next: Next,
+) -> Result<impl IntoResponse, (StatusCode, String)>
+{
+    let method = req.method();
+    let uri = req.uri();
+    eprintln!("{method} {uri}");
+
+    let headers = req.headers();
+    eprintln!("headers {headers:?}");
+
+    let (parts, body) = req.into_parts();
+    let bytes = buffer_and_print("request", body).await?;
+    let req = Request::from_parts(parts, Body::from(bytes));
+
+    let res = next.run(req).await;
+
+    let (parts, body) = res.into_parts();
+    let bytes = buffer_and_print("response", body).await?;
+    let res = Response::from_parts(parts, Body::from(bytes));
+
+    Ok(res)
+}
+
+async fn buffer_and_print<B>(
+    direction: &str, body: B
+) -> Result<Bytes, (StatusCode, String)>
+where
+    B: axum::body::HttpBody<Data = Bytes>,
+    B::Error: std::fmt::Display,
+{
+    let bytes = match body.collect().await {
+        Ok(collected) => collected.to_bytes(),
+        Err(err) => {
+            return Err((
+                StatusCode::BAD_REQUEST,
+                format!("failed to read {direction} body: {err}"),
+            ));
+        }
+    };
+
+    if let Ok(body) = std::str::from_utf8(&bytes) {
+        eprintln!("{direction} body = {body:?}");
+    }
+
+    Ok(bytes)
 }
 
 fn routes(api: &str, read_only: bool) -> Router<AppState> {
@@ -216,6 +266,7 @@ fn routes(api: &str, read_only: bool) -> Router<AppState> {
         )
         .nest(api, api_router)
         .fallback(handlers::not_found)
+        .layer(middleware::from_fn(print_request_response))
         .layer(
             ServiceBuilder::new()
                 .layer(CorsLayer::very_permissive())
