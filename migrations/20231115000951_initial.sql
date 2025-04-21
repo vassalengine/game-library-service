23ed7fca934b64df8b614d07cd0588820080c063
Joel Uckelman <uckelman@nomic.net>
2025-04-21 20:29:46 +0100

    Renumbered the flag types.

--- migrations/20231115000951_initial.sql
+++ migrations/20231115000951_initial.sql
@@ -187,8 +187,9 @@ CREATE TABLE flags(
   flag_id INTEGER PRIMARY KEY NOT NULL CHECK(flag_id >= 0),
   user_id INTEGER NOT NULL CHECK(user_id >= 0),
   project_id INTEGER NOT NULL CHECK(project_id >= 0),
-  flag INTEGER NOT NULL CHECK(flag >= 1 AND flag <= 4),
+  flag INTEGER NOT NULL CHECK(flag >= 0 AND flag <= 3),
   message TEXT,
+  CHECK(((flag == 0 OR flag == 1) AND message IS NULL) OR ((flag == 2 OR flag == 3) AND message IS NOT NULL)),
   FOREIGN KEY(user_id) REFERENCES users(user_id),
   FOREIGN KEY(project_id) REFERENCES projects(project_id)
 );
