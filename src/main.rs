470bb561e2120828df719617b0583e78180ccfb8
Joel Uckelman <uckelman@nomic.net>
2025-05-28 23:32:08 +0100

    Log to file, rotated daily.

--- src/main.rs
+++ src/main.rs
@@ -360,6 +360,10 @@ async fn run() -> Result<(), StartupError> {
 #[tokio::main]
 async fn main() {
     // set up logging
+    // TODO: make log location configurable
+    let file_appender = tracing_appender::rolling::daily("", "gls.log");
+    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+
     tracing_subscriber::registry()
         .with(EnvFilter::try_from_default_env()
             .unwrap_or_else(|_| {
@@ -375,7 +379,10 @@ async fn main() {
                 ].join(",").into()
             }),
         )
-        .with(tracing_subscriber::fmt::layer().with_target(false))
+        .with(tracing_subscriber::fmt::layer()
+            .with_target(false)
+            .with_writer(non_blocking)
+        )
         .init();
 
     // ensure that panics are logged
