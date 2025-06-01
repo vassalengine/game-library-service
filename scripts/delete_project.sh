bc49860f2ae6b956852419ad7307f7b9422925d3
Joel Uckelman <uckelman@nomic.net>
2025-06-01 15:44:49 +0100

    Added script for deleting projects.

---
+++ scripts/delete_project.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+proj_id="$1"
+
+cat << EOF | sqlite3 projects.db
+DELETE FROM flags WHERE project_id = $proj_id;
+DELETE FROM tags WHERE project_id = $proj_id;
+DELETE FROM project_revisions WHERE project_id = $proj_id;
+DELETE FROM project_data WHERE project_id = $proj_id;
+DELETE FROM image_revisions WHERE project_id = $proj_id;
+DELETE FROM galleries WHERE project_id = $proj_id;
+DELETE FROM players WHERE project_id = $proj_id;
+DELETE FROM owners WHERE project_id = $proj_id;
+DELETE FROM authors WHERE release_id IN (SELECT releases.release_id FROM releases JOIN packages ON releases.package_id = packages.package_id WHERE packages.project_id = $proj_id);
+DELETE FROM files WHERE release_id IN (SELECT releases.release_id FROM releases JOIN packages ON releases.package_id = packages.package_id WHERE packages.project_id = $proj_id);
+DELETE FROM releases WHERE package_id IN (SELECT package_id FROM packages WHERE project_id = $proj_id);
+DELETE FROM packages WHERE project_id = $proj_id;
+UPDATE projects SET image = NULL WHERE project_id = $proj_id;
+DELETE FROM images WHERE project_id = $proj_id;
+DELETE FROM projects WHERE project_id = $proj_id;
+EOF
