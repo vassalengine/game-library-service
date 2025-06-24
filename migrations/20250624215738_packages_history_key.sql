37902bd073dd6e7852bf4788016b501a5ed4d582
Joel Uckelman <uckelman@nomic.net>
2025-06-25 00:14:36 +0100

    Use the right pragma.

--- migrations/20250624215738_packages_history_key.sql
+++ migrations/20250624215738_packages_history_key.sql
@@ -1,6 +1,6 @@
 COMMIT TRANSACTION;
 
-PRAGMA defer_foreign_keys = ON;
+PRAGMA foreign_keys = OFF;
 
 BEGIN TRANSACTION;
 
@@ -24,6 +24,6 @@ ALTER TABLE tmp RENAME TO packages;
 
 COMMIT TRANSACTION;
 
-PRAGMA defer_foreign_keys = OFF;
+PRAGMA foreign_keys = ON;
 
 BEGIN TRANSACTION;
