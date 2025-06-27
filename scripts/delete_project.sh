ee6d3de6bc5c6c14442c71d8cafaebdd1c49748c
Joel Uckelman <uckelman@nomic.net>
2025-06-27 18:20:28 +0000

    Added packages_history, releases_history tables. Do project deletion in a transaction for safety.

--- scripts/delete_project.sh
+++ scripts/delete_project.sh
@@ -3,6 +3,8 @@
 proj_id="$1"
 
 cat << EOF | sqlite3 projects.db
+PRAGMA foreign_keys = ON;
+BEGIN TRANSACTION;
 DELETE FROM flags WHERE project_id = $proj_id;
 DELETE FROM tags WHERE project_id = $proj_id;
 DELETE FROM project_revisions WHERE project_id = $proj_id;
@@ -13,8 +15,11 @@ DELETE FROM players WHERE project_id = $proj_id;
 DELETE FROM owners WHERE project_id = $proj_id;
 DELETE FROM files WHERE release_id IN (SELECT releases.release_id FROM releases JOIN packages ON releases.package_id = packages.package_id WHERE packages.project_id = $proj_id);
 DELETE FROM releases WHERE package_id IN (SELECT package_id FROM packages WHERE project_id = $proj_id);
+DELETE FROM releases_history WHERE package_id IN (SELECT package_id FROM packages_history WHERE project_id = $proj_id);
 DELETE FROM packages WHERE project_id = $proj_id;
+DELETE FROM packages_history WHERE project_id = $proj_id;
 UPDATE projects SET image = NULL WHERE project_id = $proj_id;
 DELETE FROM images WHERE project_id = $proj_id;
 DELETE FROM projects WHERE project_id = $proj_id;
+COMMIT TRANSACTION;
 EOF
