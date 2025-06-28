794002048cd05831eb4d9e05a0dbc7da7242622b
Joel Uckelman <uckelman@nomic.net>
2025-06-28 22:33:49 +0000

    Updated user merge script.

--- scripts/merge_user.sh
+++ scripts/merge_user.sh
@@ -4,10 +4,16 @@ src="$1"
 dst="$2"
 
 cat << EOF | sqlite3 projects.db
+PRAGMA foreign_keys = ON;
+BEGIN TRANSACTION;
 UPDATE OR IGNORE owners SET user_id = $dst WHERE user_id = $src;
 UPDATE OR IGNORE players SET user_id = $dst WHERE user_id = $src;
 UPDATE packages SET created_by = $dst WHERE created_by = $src;
+UPDATE packages_history SET created_by = $dst WHERE created_by = $src;
+UPDATE packages_history SET deleted_by = $dst WHERE deleted_by = $src;
 UPDATE releases SET published_by = $dst WHERE published_by = $src;
+UPDATE releases_history SET published_by = $dst WHERE published_by = $src;
+UPDATE releases_history SET deleted_by = $dst WHERE deleted_by = $src;
 UPDATE files SET published_by = $dst WHERE published_by = $src;
 UPDATE images SET published_by = $dst WHERE published_by = $src;
 UPDATE image_revisions SET published_by = $dst WHERE published_by = $src;
@@ -17,4 +23,5 @@ UPDATE projects SET modified_by = $dst WHERE modified_by = $src;
 UPDATE project_revisions SET modified_by = $dst WHERE modified_by = $src;
 UPDATE flags SET user_id = $dst WHERE user_id = $src;
 DELETE FROM users WHERE user_id = $src;
+END TRANSACTION;
 EOF
