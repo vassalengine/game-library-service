5fb593a1c5aceb2b35d92574db0757b70c3c2eb3
Joel Uckelman <uckelman@nomic.net>
2025-10-22 12:02:44 +0100

    Consolidated db schema.

--- migrations/20251022105942_consolidated.sql
+++ migrations/20251022105942_consolidated.sql
@@ -144,7 +144,10 @@ CREATE TABLE IF NOT EXISTS image_revisions (
   UNIQUE(project_id, filename, published_at)
 );
 
-CREATE TABLE IF NOT EXISTS galleries (
+CREATE TABLE IF NOT EXISTS galleries_history (
+  gallery_id INTEGER PRIMARY KEY NOT NULL CHECK(gallery_id >= 0),
+  prev_id INTEGER REFERENCES galleries_history(gallery_id),
+  next_id INTEGER REFERENCES galleries_history(gallery_id),
   project_id INTEGER NOT NULL,
   filename TEXT NOT NULL,
   description TEXT NOT NULL,
@@ -152,17 +155,35 @@ CREATE TABLE IF NOT EXISTS galleries (
   published_by INTEGER NOT NULL,
   removed_at INTEGER,
   removed_by INTEGER,
-  position INTEGER NOT NULL,
   FOREIGN KEY(project_id) REFERENCES projects(project_id),
   FOREIGN KEY(published_by) REFERENCES users(user_id),
   FOREIGN KEY(removed_by) REFERENCES users(user_id),
   FOREIGN KEY(project_id, filename) REFERENCES images(project_id, filename),
   UNIQUE(project_id, filename),
-  UNIQUE(project_id, position),
+  CHECK(next_id != gallery_id),
+  CHECK(prev_id != gallery_id),
   CHECK(
     (removed_at IS NULL AND removed_by IS NULL) OR
     (removed_at >= published_at AND removed_by IS NOT NULL)
   )
+);
+
+CREATE TABLE IF NOT EXISTS galleries (
+  gallery_id INTEGER PRIMARY KEY NOT NULL,
+  prev_id INTEGER REFERENCES galleries(gallery_id),
+  next_id INTEGER REFERENCES galleries(gallery_id),
+  project_id INTEGER NOT NULL,
+  filename TEXT NOT NULL,
+  description TEXT NOT NULL,
+  published_at INTEGER NOT NULL,
+  published_by INTEGER NOT NULL,
+  FOREIGN KEY(gallery_id) REFERENCES galleries_history(gallery_id),
+  FOREIGN KEY(project_id) REFERENCES projects(project_id),
+  FOREIGN KEY(published_by) REFERENCES users(user_id),
+  FOREIGN KEY(project_id, filename) REFERENCES images(project_id, filename),
+  UNIQUE(project_id, filename),
+  CHECK(prev_id != gallery_id),
+  CHECK(next_id != gallery_id)
 );
 
 CREATE TABLE IF NOT EXISTS tags (
