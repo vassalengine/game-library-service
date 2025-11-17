ec13373f848657808301cd403a00643029391d1f
Joel Uckelman <uckelman@nomic.net>
2025-11-17 14:50:32 +0000

    Consolidated db schema.

--- game-library-service/migrations/20251117144008_consolidated.sql
+++ game-library-service/migrations/20251117144008_consolidated.sql
@@ -204,7 +204,7 @@ END;
 CREATE TABLE IF NOT EXISTS galleries_history (
   gallery_id INTEGER PRIMARY KEY NOT NULL CHECK(gallery_id >= 0),
   project_id INTEGER NOT NULL,
-  sort_key BLOB NOT NULL,
+  sort_key BLOB NOT NULL CHECK(LENGTH(sort_key) > 0),
   filename TEXT NOT NULL,
   description TEXT NOT NULL,
   published_at INTEGER NOT NULL,
@@ -224,7 +224,7 @@ CREATE TABLE IF NOT EXISTS galleries_history (
 CREATE TABLE IF NOT EXISTS galleries (
   gallery_id INTEGER PRIMARY KEY NOT NULL,
   project_id INTEGER NOT NULL,
-  sort_key BLOB NOT NULL,
+  sort_key BLOB NOT NULL CHECK(LENGTH(sort_key) > 0),
   filename TEXT NOT NULL,
   description TEXT NOT NULL,
   published_at INTEGER NOT NULL,
@@ -268,11 +268,56 @@ BEGIN
 END;
 
 CREATE TABLE IF NOT EXISTS tags (
-  project_id INTEGER NOT NULL,
-  tag TEXT NOT NULL,
-  FOREIGN KEY(project_id) REFERENCES projects(project_id),
-  UNIQUE(project_id, tag)
+  tag_id INTEGER PRIMARY KEY NOT NULL CHECK(tag_id >= 0),
+  tag TEXT NOT NULL CHECK(tag != ""),
+  UNIQUE(tag)
 );
+
+CREATE TABLE IF NOT EXISTS projects_tags (
+  project_id INTEGER NOT NULL,
+  tag_id INTEGER NOT NULL,
+  FOREIGN KEY(project_id) REFERENCES projects(project_id),
+  FOREIGN KEY(tag_id) REFERENCES tags(tag_id),
+  UNIQUE(project_id, tag_id)
+);
+
+CREATE TABLE IF NOT EXISTS projects_tags_history (
+  project_id INTEGER NOT NULL,
+  tag_id INTEGER NOT NULL,
+  added_at INTEGER NOT NULL,
+  added_by INTEGER NOT NULL,
+  removed_at INTEGER,
+  removed_by INTEGER,
+  FOREIGN KEY(project_id) REFERENCES projects_history(project_id),
+  FOREIGN KEY(tag_id) REFERENCES tags(tag_id),
+  FOREIGN KEY(added_by) REFERENCES users(user_id),
+  FOREIGN KEY(removed_by) REFERENCES users(user_id),
+  CHECK(
+    (removed_at IS NULL AND removed_by IS NULL) OR
+    (removed_at >= added_at AND removed_by IS NOT NULL)
+  )
+);
+
+CREATE TRIGGER IF NOT EXISTS projects_tags_history_ai_start
+AFTER INSERT ON projects_tags_history
+BEGIN
+  INSERT INTO projects_tags (
+    project_id,
+    tag_id
+  )
+  VALUES (
+    NEW.project_id,
+    NEW.tag_id
+  );
+END;
+
+CREATE TRIGGER IF NOT EXISTS projects_tags_history_au_end
+AFTER UPDATE OF removed_at ON projects_tags_history
+BEGIN
+  DELETE FROM projects_tags
+  WHERE project_id = OLD.project_id
+    AND tag_id = OLD.tag_id;
+END;
 
 CREATE TABLE IF NOT EXISTS projects_history (
   project_id INTEGER PRIMARY KEY NOT NULL CHECK(project_id >= 0),
