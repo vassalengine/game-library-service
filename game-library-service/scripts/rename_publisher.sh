117602e39f0b9955532620c8a510d650fd336410
Joel Uckelman <uckelman@nomic.net>
2025-11-21 20:17:18 +0000

    Fixed publisher rename script. We have to temporarily suspend the foreign key constraints to update the publisher name.

--- game-library-service/scripts/rename_publisher.sh
+++ game-library-service/scripts/rename_publisher.sh
@@ -4,9 +4,11 @@ pid="$1"
 name="$2"
 
 cat << EOF | sqlite3 projects.db
-PRAGMA foreign_keys = ON;
+PRAGMA foreign_keys = OFF;
 BEGIN TRANSACTION;
-UPDATE projects SET game_publisher = 'name' WHERE game_publisher_id = $pid;
+UPDATE projects SET game_publisher = '$name' WHERE game_publisher_id = $pid;
 UPDATE publishers SET name = '$name' WHERE publisher_id = $pid;
 END TRANSACTION;
+PRAGMA foreign_keys = ON;
+PRAGMA foreign_key_check;
 EOF
